a143fae95b4b78b5070c9decb81a26600e02189d
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-24 20:56:04 -0300

    Add project files.

---
+++ XF.MVVMBasic/XF.MVVMBasic/Business/AlunoBusiness.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XF.MVVMBasic.Model;
+using XF.MVVMBasic.Repository;
+
+namespace XF.MVVMBasic.Business
+{
+    public class AlunoBusiness
+    {
+        public List<Aluno> GetAlunos()
+        {
+            AlunoRepository repository = new AlunoRepository();
+
+            return repository.GetAlunos();
+        }
+    }
+}
---
+++ XF.MVVMBasic/XF.MVVMBasic/Repository/AlunoRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XF.MVVMBasic.Model;
+
+namespace XF.MVVMBasic.Repository
+{
+    public class AlunoRepository
+    {
+        public List<Aluno> GetAlunos()
+        {
+            List<Aluno> alunos = new List<Aluno>();
+
+            alunos.Add(new Aluno() { Id = Guid.NewGuid(), RM = "313131", Nome = "Priscila", Email = "priscila@teste.com" });
+            alunos.Add(new Aluno() { Id = Guid.NewGuid(), RM = "313131", Nome = "Priscila", Email = "priscila@teste.com" });
+            alunos.Add(new Aluno() { Id = Guid.NewGuid(), RM = "313131", Nome = "Priscila", Email = "priscila@teste.com" });
+            alunos.Add(new Aluno() { Id = Guid.NewGuid(), RM = "313131", Nome = "Priscila", Email = "priscila@teste.com" });
+
+            return alunos;
+        }
+    }
+}
